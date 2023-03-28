fbb1c694e7f6e6c7408b04a7f851ca0f7f17dc3f
Alfany Riza Mahendra <70315533+alfanyRizaMahendra@users.noreply.github.com>
2023-03-28 12:25:40 +0000

    progress

--- app.py
+++ app.py
@@ -1,11 +1,31 @@
-from flask import Flask
+from flask import Flask  #Flask class
+from flask import render_template  #to render all templates
 
 app = Flask(__name__)  # define class
+
+pekerjaan = [{
+  'id': 1,
+  'title': 'Data Scientist',
+  'location': 'Delhi, India',
+  'salary': 'Rs. 15,000,000'
+}, {
+  'id': 2,
+  'title': 'Data Engineer',
+  'location': 'Singapore',
+  'salary': '$.15,000'
+}, {
+  'id': 3,
+  'title': 'Business Analyst',
+  'location': 'Indonesia',
+  'salary': 'Rp. 15,000,000'
+}]
 
 
 @app.route("/")
 def hello_world():
-  return "Hello World"
+  return render_template(
+    'home.html', job=pekerjaan
+  )  #jobs is going to make JOBS dictionary can be access in html file -- showing dynamic data
 
 
 if __name__ == "__main__":
