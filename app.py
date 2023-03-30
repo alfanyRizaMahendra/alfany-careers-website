98917f7b5ab58eb1fa39759523f7e9e2fa0253b6
Alfany Riza Mahendra <70315533+alfanyRizaMahendra@users.noreply.github.com>
2023-03-30 00:32:59 +0000

    v.0.3

--- app.py
+++ app.py
@@ -1,18 +1,17 @@
 from flask import Flask  #Flask class
 from flask import render_template  #to render all templates
+from flask import jsonify # To convert dictionary text to json
 
 app = Flask(__name__)  # define class
 
 pekerjaan = [{
   'id': 1,
   'title': 'Data Scientist',
-  'location': 'Delhi, India',
   'salary': 'Rs. 15,000,000'
 }, {
   'id': 2,
   'title': 'Data Engineer',
   'location': 'Singapore',
-  'salary': '$.15,000'
 }, {
   'id': 3,
   'title': 'Business Analyst',
@@ -21,12 +20,15 @@ pekerjaan = [{
 }]
 
 
-@app.route("/")
+@app.route("/") # don't add any new url
 def hello_world():
   return render_template(
-    'home.html', job=pekerjaan
+    'home.html', job=pekerjaan, company_name="PAKO"
   )  #jobs is going to make JOBS dictionary can be access in html file -- showing dynamic data
 
-
+@app.route("/job") # add "/job" url to access dictionary data (json format -- restAPI)
+def list_jobs():
+  return jsonify(pekerjaan)
+  
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
